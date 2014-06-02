1fd1b715577a7d08f3267204095be03d5ece0a46
paxal <cpascal@igraal.com>
2014-06-02 17:27:48 +0200

    Add download helpers

--- src/IgraalOSB/StatsTableBundle/Response/StatsTableResponse.php
+++ src/IgraalOSB/StatsTableBundle/Response/StatsTableResponse.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class StatsTableResponse extends Response
 {
+    const ST_HEADER_CONTENT_DISPOSITION = 'content-disposition';
+    const ST_HEADER_CONTENT_TYPE        = 'content-type';
+
     /** @var StatsTable */
     private $statsTable;
     /** @var DumperInterface */
@@ -20,8 +23,8 @@ class StatsTableResponse extends Response
         $this->statsTable = $statsTable;
         $this->dumper = $dumper;
 
-        if (!$this->headers->has('content-type')) {
-            $this->headers->set('content-type', $dumper->getMimeType());
+        if (!$this->headers->has(self::ST_HEADER_CONTENT_TYPE)) {
+            $this->headers->set(self::ST_HEADER_CONTENT_TYPE, $dumper->getMimeType());
         }
     }
 
@@ -68,5 +71,25 @@ class StatsTableResponse extends Response
         }
 
         return $this->content;
+    }
+
+    /**
+     * Ask the browser to download the file with specific filename
+     *
+     * @param string $filename
+     */
+    public function askDownload($filename = 'stats')
+    {
+        $contentDisposition = 'attachement';
+        if ($filename) {
+            $contentDisposition .= sprintf('filename="%s"', addslashes($filename));
+        }
+
+        $this->headers->set(self::ST_HEADER_CONTENT_DISPOSITION, $contentDisposition);
+    }
+
+    public function noDownload()
+    {
+        $this->headers->remove(self::ST_HEADER_CONTENT_DISPOSITION);
     }
 }
